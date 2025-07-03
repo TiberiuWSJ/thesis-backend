93a0dcaff3b85bb8a0086750b6fb973bcfc6667f
TiberiuWSJ <96244392+TiberiuWSJ@users.noreply.github.com>
2025-07-03 05:27:14 +0300

    added frontend adaptations 5

--- app/main.py
+++ app/main.py
@@ -1,15 +1,27 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 from app.database import init_db
 from app.routers.users import router as users_router
 from app.routers.scenes import router as scenes_router
 from app.routers.debug import router as debug_router
-
 
 app = FastAPI(
     title="Interior-3D API",
     version="0.1.0",
 )
 
+# Allow requests coming from your Next.js front-end origin(s)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:3000",          # if you ever browse locally
+        "http://10.220.202.24:3000"       # your EC2‐in‐VPC front-end URL
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.on_event("startup")
 def on_startup():
